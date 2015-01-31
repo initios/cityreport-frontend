41ff1098132d0649931da0fd05e084662ebe717a
Hector <hector.fernandez.cascallar@gmail.com>
2015-01-31 13:21:46 +0100

    types list for testing

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,10 +9,15 @@ angular.module('starter.controllers', [])
 
 .controller('NewIssueCtrl', function($scope){
 
+    $scope.issuesTypes = {
+        0: {id: 1, name: 'type 1'},
+        1: {id: 2, name: 'type 2'},
+        2: {id: 3, name: 'type 3'}
+    }
 
 })
 
 .controller('IssuesCtrl', function($scope){
-    
+
 
 });
